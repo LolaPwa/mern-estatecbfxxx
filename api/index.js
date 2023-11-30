b8785c02209509c969c8d1011bf461c9158a72e8
Lola Pwasanga <lola.pwasanga@gmail.com>
2023-11-30 08:04:45 +0000

    update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+//import listingRouter from './routes/listing.route.js';
+import cookieParser from'cookie-parser';
+import path from 'path';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() =>{
@@ -11,8 +15,11 @@ mongoose.connect(process.env.MONGO).then(() =>{
 .catch((err) => {
     console.log(err);
 });
+
+
 const app=express();
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,11 @@
 import express from 'express';
-import {test} from '../controllers/user.controller.js';
-
+import {test,  updateUser} from '../controllers/user.controller.js';
+import {verifyToken} from '../utils/verifyUser.js';
 const router = express.Router();
+
 router.get('/test',test);
+router.post('/update/:id', verifyToken, updateUser)
+//router.post('/delete/:id', verifyToken, deleteUser)
+//router.post('/listings/:id', verifyToken, getUserListings)
+//router.post('/:id', verifyToken, getUser)
 export default router;
