6fd1f57de8944f0e389e9d287cbc5bf31cc2091d
Lola Pwasanga <lola.pwasanga@gmail.com>
2024-01-03 18:57:10 +0000

    TEST errors

--- api/__tests__/server.test.js
+++ api/__tests__/server.test.js
@@ -1,20 +1,29 @@
 import request from "supertest";
 import app from "../index.js";
+import {setupbeforeAll, setupafterAll} from "../../setupTests.js";
+
+beforeAll(async () => {
+  await setupbeforeAll();
+});
+
+afterAll(async () => {
+    await setupafterAll();
+});
 
 describe('Property Listing App', () => {
     it("should fetch all properties", async ()=> {
-        const response = await request(app).get('/api/listings');
+        const response = await request(app).get('/api/listing');
         expect(response.status).toBe(200);
         expect(response.body).toHaveLength(0);
     });
 
     it("should add a new property", async ()=> {
         const newProperty = {name: 'Sample Property', regularPrice:10000};
-        const response = await request(app).post('/api/listing').send(newListing);
+        const response = await request(app).post('/api/listing').send(newProperty);
         expect(response.status).toBe(201);
         expect(response.body).toMatchObject(newProperty);
   
-        const properties= await request(app).get('/api/listings');
+        const properties= await request(app).get('/api/listing');
         expect(properties.body).toHaveLength(1);
         expect(properties.body[0]).toMatchObject(newProperty);
 
--- setupTests.js
+++ setupTests.js
@@ -2,24 +2,27 @@ import mongoose  from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 let mongod;
 
-beforeAll(async() => {
+export const setupbeforeAll= async() => {
+  if(mongoose.connection.readyState !== 0) {
+    await mongoose.disconnect();
+  }
     mongod = await MongoMemoryServer.create();
     const uri = mongod.getUri();
    
     // set the MongoDb URI to use the in-memory server during  tests
     process.env.MONGODB_URI = uri;
 
-    await mongoose.connect(uri, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-  });
+  };
 
 
-afterAll(async () => {
+export const setupafterAll=async () => {
     await mongoose.disconnect();
     // stop the MongoDb memory server
     if (mongod) {
       await mongod.stop();
     }
-});
+};
